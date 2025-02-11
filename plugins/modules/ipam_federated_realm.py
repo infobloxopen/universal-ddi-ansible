3e15e42839ad9a9a15210b132b44f3e3f1c29dee
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-02-11 16:00:23 +0530

    Initial commit

---
+++ plugins/modules/ipam_federated_realm.py
@@ -0,0 +1,309 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: Infoblox Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ipam_federated_realm
+short_description: Manage FederatedRealm
+description:
+    - Manage FederatedRealm
+version_added: 2.0.0
+author: Infoblox Inc. (@infobloxopen)
+options:
+    id:
+        description:
+            - ID of the object
+        type: str
+        required: false
+    state:
+        description:
+            - Indicate desired state of the object
+        type: str
+        required: false
+        choices:
+            - present
+            - absent
+        default: present
+    allocation_v4:
+        description:
+            - "The aggregate of all Federated Blocks within the Realm."
+        type: dict
+        suboptions:
+            allocated:
+                description:
+                    - "Percent of total space allocated."
+                type: int
+            delegated:
+                description:
+                    - "Percent of total space delegated."
+                type: int
+            overlapping:
+                description:
+                    - "Percent of total space in overlapping blocks."
+                type: int
+            reserved:
+                description:
+                    - "Percent of total space reserved."
+                type: int
+    comment:
+        description:
+            - "The description of the federated realm. May contain 0 to 1024 characters. Can include UTF-8."
+        type: str
+    name:
+        description:
+            - "The name of the federated realm. May contain 1 to 256 characters; can include UTF-8."
+        type: str
+        required: true
+    tags:
+        description:
+            - "The tags for the federated realm in JSON format."
+        type: dict
+
+extends_documentation_fragment:
+    - infoblox.universal_ddi.common
+"""  # noqa: E501
+
+EXAMPLES = r"""
+    - name: Create a Federated Realm
+      infoblox.universal_ddi.ipam_federated_realm:
+        name: "{{ fed_realm_name }}"
+        state: "present"
+      register: federated_realm
+
+    - name: Create a Federated Realm with Additional Parameters
+      infoblox.universal_ddi.ipam_federated_realm:
+        name: "{{ fed_realm_name }}"
+        comment: "This is a federated realm"
+        state: "present"
+        tags:
+            location: "site-1"
+      register: federated_realm
+
+    - name: Delete the Federated Realm
+      infoblox.universal_ddi.ipam_federated_realm:
+        name: "{{ fed_realm_name }}"
+        state: "absent"
+"""
+
+RETURN = r"""
+id:
+    description:
+        - ID of the FederatedRealm object
+    type: str
+    returned: Always
+item:
+    description:
+        - FederatedRealm object
+    type: complex
+    returned: Always
+    contains:
+        allocation_v4:
+            description:
+                - "The aggregate of all Federated Blocks within the Realm."
+            type: dict
+            returned: Always
+            contains:
+                allocated:
+                    description:
+                        - "Percent of total space allocated."
+                    type: int
+                    returned: Always
+                delegated:
+                    description:
+                        - "Percent of total space delegated."
+                    type: int
+                    returned: Always
+                overlapping:
+                    description:
+                        - "Percent of total space in overlapping blocks."
+                    type: int
+                    returned: Always
+                reserved:
+                    description:
+                        - "Percent of total space reserved."
+                    type: int
+                    returned: Always
+        comment:
+            description:
+                - "The description of the federated realm. May contain 0 to 1024 characters. Can include UTF-8."
+            type: str
+            returned: Always
+        created_at:
+            description:
+                - "Time when the object has been created."
+            type: str
+            returned: Always
+        id:
+            description:
+                - "The resource identifier."
+            type: str
+            returned: Always
+        name:
+            description:
+                - "The name of the federated realm. May contain 1 to 256 characters; can include UTF-8."
+            type: str
+            returned: Always
+        tags:
+            description:
+                - "The tags for the federated realm in JSON format."
+            type: dict
+            returned: Always
+        updated_at:
+            description:
+                - "Time when the object has been updated. Equals to I(created_at) if not updated after creation."
+            type: str
+            returned: Always
+"""  # noqa: E501
+
+from ansible_collections.infoblox.universal_ddi.plugins.module_utils.modules import UniversalDDIAnsibleModule
+
+try:
+    from ipam_federation import FederatedRealm, FederatedRealmApi
+    from universal_ddi_client import ApiException, NotFoundException
+except ImportError:
+    pass  # Handled by UniversalDDIAnsibleModule
+
+
+class FederatedRealmModule(UniversalDDIAnsibleModule):
+    def __init__(self, *args, **kwargs):
+        super(FederatedRealmModule, self).__init__(*args, **kwargs)
+
+        exclude = ["state", "csp_url", "api_key", "portal_url", "portal_key", "id"]
+        self._payload_params = {k: v for k, v in self.params.items() if v is not None and k not in exclude}
+        self._payload = FederatedRealm.from_dict(self._payload_params)
+        self._existing = None
+
+    @property
+    def existing(self):
+        return self._existing
+
+    @existing.setter
+    def existing(self, value):
+        self._existing = value
+
+    @property
+    def payload_params(self):
+        return self._payload_params
+
+    @property
+    def payload(self):
+        return self._payload
+
+    def payload_changed(self):
+        if self.existing is None:
+            # if existing is None, then it is a create operation
+            return True
+
+        return self.is_changed(self.existing.model_dump(by_alias=True, exclude_none=True), self.payload_params)
+
+    def find(self):
+        if self.params["id"] is not None:
+            try:
+                resp = FederatedRealmApi(self.client).read(self.params["id"], inherit="full")
+                return resp.result
+            except NotFoundException as e:
+                if self.params["state"] == "absent":
+                    return None
+                raise e
+        else:
+            filter = f"name=='{self.params['name']}'"
+            resp = FederatedRealmApi(self.client).list(filter=filter)
+
+            # If no results, set results to empty list
+            if not resp.results:
+                resp.results = []
+
+            if len(resp.results) == 1:
+                return resp.results[0]
+            if len(resp.results) > 1:
+                self.fail_json(msg=f"Found multiple FederatedRealm: {resp.results}")
+            if len(resp.results) == 0:
+                return None
+
+    def create(self):
+        if self.check_mode:
+            return None
+
+        resp = FederatedRealmApi(self.client).create(body=self.payload)
+        return resp.result.model_dump(by_alias=True, exclude_none=True)
+
+    def update(self):
+        if self.check_mode:
+            return None
+
+        resp = FederatedRealmApi(self.client).update(id=self.existing.id, body=self.payload)
+        return resp.result.model_dump(by_alias=True, exclude_none=True)
+
+    def delete(self):
+        if self.check_mode:
+            return
+
+        FederatedRealmApi(self.client).delete(self.existing.id)
+
+    def run_command(self):
+        result = dict(changed=False, object={}, id=None)
+
+        # based on the state that is passed in, we will execute the appropriate
+        # functions
+        try:
+            self.existing = self.find()
+            item = {}
+            if self.params["state"] == "present" and self.existing is None:
+                item = self.create()
+                result["changed"] = True
+                result["msg"] = "FederatedRealm created"
+            elif self.params["state"] == "present" and self.existing is not None:
+                if self.payload_changed():
+                    item = self.update()
+                    result["changed"] = True
+                    result["msg"] = "FederatedRealm updated"
+            elif self.params["state"] == "absent" and self.existing is not None:
+                self.delete()
+                result["changed"] = True
+                result["msg"] = "FederatedRealm deleted"
+
+            if self.check_mode:
+                # if in check mode, do not update the result or the diff, just return the changed state
+                self.exit_json(**result)
+
+            result["diff"] = dict(
+                before=self.existing.model_dump(by_alias=True, exclude_none=True) if self.existing is not None else {},
+                after=item,
+            )
+            result["object"] = item
+            result["id"] = (
+                self.existing.id if self.existing is not None else item["id"] if (item and "id" in item) else None
+            )
+        except ApiException as e:
+            self.fail_json(msg=f"Failed to execute command: {e.status} {e.reason} {e.body}")
+
+        self.exit_json(**result)
+
+
+def main():
+    module_args = dict(
+        id=dict(type="str", required=False),
+        state=dict(type="str", required=False, choices=["present", "absent"], default="present"),
+        allocation_v4=dict(type="dict", options=dict()),
+        comment=dict(type="str"),
+        name=dict(type="str", required=True),
+        tags=dict(type="dict"),
+    )
+
+    module = FederatedRealmModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+        required_if=[("state", "present", ["name"])],
+    )
+
+    module.run_command()
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/modules/ipam_federated_realm_info.py
@@ -0,0 +1,251 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: Infoblox Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ipam_federated_realm_info
+short_description: Retrieves the FederatedRealm 
+description:
+    - Retrieves the FederatedRealm  
+version_added: 2.0.0
+author: Infoblox Inc. (@infobloxopen)
+options:
+    id:
+        description:
+            - ID of the object
+        type: str
+        required: false
+    filters:
+        description:
+            - Filter dict to filter objects
+        type: dict
+        required: false
+    filter_query:
+        description:
+            - Filter query to filter objects
+        type: str
+        required: false
+    inherit:
+        description:
+            - Return inheritance information
+        type: str
+        required: false
+        choices:
+            - full
+            - partial
+            - none
+        default: full
+    tag_filters:
+        description:
+            - Filter dict to filter objects by tags
+        type: dict
+        required: false
+    tag_filter_query:
+        description:
+            - Filter query to filter objects by tags
+        type: str
+        required: false
+
+extends_documentation_fragment:
+    - infoblox.universal_ddi.common
+"""  # noqa: E501
+
+EXAMPLES = r"""
+    - name: Get Information about the Federated Realm by ID
+      infoblox.universal_ddi.ipam_federated_realm_info:
+        id: "{{ federated_realm.id }}"
+
+    - name: Get Information about the Federated Realm by filters (Name)
+      infoblox.universal_ddi.ipam_federated_realm_info:
+        filters:
+          name: "example_federated_realm"
+
+    - name: Get Information about the Federated Realm by filter query
+      infoblox.universal_ddi.ipam_federated_realm_info:
+        filter_query: "name=='example_federated_realm'"
+
+    - name: Get Information about the Federated Realm by tag filters
+      infoblox.universal_ddi.ipam_federated_realm_info:
+        tag_filters:
+          location: "site-1"
+"""
+
+RETURN = r"""
+id:
+    description:
+        - ID of the FederatedRealm object
+    type: str
+    returned: Always
+objects:
+    description:
+        - FederatedRealm object
+    type: list
+    elements: dict
+    returned: Always
+    contains:
+        allocation_v4:
+            description:
+                - "The aggregate of all Federated Blocks within the Realm."
+            type: dict
+            returned: Always
+            contains:
+                allocated:
+                    description:
+                        - "Percent of total space allocated."
+                    type: int
+                    returned: Always
+                delegated:
+                    description:
+                        - "Percent of total space delegated."
+                    type: int
+                    returned: Always
+                overlapping:
+                    description:
+                        - "Percent of total space in overlapping blocks."
+                    type: int
+                    returned: Always
+                reserved:
+                    description:
+                        - "Percent of total space reserved."
+                    type: int
+                    returned: Always
+        comment:
+            description:
+                - "The description of the federated realm. May contain 0 to 1024 characters. Can include UTF-8."
+            type: str
+            returned: Always
+        created_at:
+            description:
+                - "Time when the object has been created."
+            type: str
+            returned: Always
+        id:
+            description:
+                - "The resource identifier."
+            type: str
+            returned: Always
+        name:
+            description:
+                - "The name of the federated realm. May contain 1 to 256 characters; can include UTF-8."
+            type: str
+            returned: Always
+        tags:
+            description:
+                - "The tags for the federated realm in JSON format."
+            type: dict
+            returned: Always
+        updated_at:
+            description:
+                - "Time when the object has been updated. Equals to I(created_at) if not updated after creation."
+            type: str
+            returned: Always
+"""  # noqa: E501
+
+from ansible_collections.infoblox.universal_ddi.plugins.module_utils.modules import UniversalDDIAnsibleModule
+
+try:
+    from ipam_federation import FederatedRealmApi
+    from universal_ddi_client import ApiException, NotFoundException
+except ImportError:
+    pass  # Handled by UniversalDDIAnsibleModule
+
+
+class FederatedRealmInfoModule(UniversalDDIAnsibleModule):
+    def __init__(self, *args, **kwargs):
+        super(FederatedRealmInfoModule, self).__init__(*args, **kwargs)
+        self._existing = None
+        self._limit = 1000
+
+    def find_by_id(self):
+        try:
+            resp = FederatedRealmApi(self.client).read(self.params["id"], inherit="full")
+            return [resp.result]
+        except NotFoundException as e:
+            return None
+
+    def find(self):
+        if self.params["id"] is not None:
+            return self.find_by_id()
+
+        filter_str = None
+        if self.params["filters"] is not None:
+            filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["filters"].items()])
+        elif self.params["filter_query"] is not None:
+            filter_str = self.params["filter_query"]
+
+        tag_filter_str = None
+        if self.params["tag_filters"] is not None:
+            tag_filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["tag_filters"].items()])
+        elif self.params["tag_filter_query"] is not None:
+            tag_filter_str = self.params["tag_filter_query"]
+
+        all_results = []
+        offset = 0
+
+        while True:
+            try:
+                resp = FederatedRealmApi(self.client).list(
+                    offset=offset, limit=self._limit, filter=filter_str, tfilter=tag_filter_str
+                )
+
+                # If no results, set results to empty list
+                if not resp.results:
+                    resp.results = []
+
+                all_results.extend(resp.results)
+
+                if len(resp.results) < self._limit:
+                    break
+                offset += self._limit
+
+            except ApiException as e:
+                self.fail_json(msg=f"Failed to execute command: {e.status} {e.reason} {e.body}")
+
+        return all_results
+
+    def run_command(self):
+        result = dict(objects=[])
+
+        if self.check_mode:
+            self.exit_json(**result)
+
+        find_results = self.find()
+
+        all_results = []
+        for r in find_results:
+            all_results.append(r.model_dump(by_alias=True, exclude_none=True))
+
+        result["objects"] = all_results
+        self.exit_json(**result)
+
+
+def main():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        id=dict(type="str", required=False),
+        filters=dict(type="dict", required=False),
+        filter_query=dict(type="str", required=False),
+        inherit=dict(type="str", required=False, choices=["full", "partial", "none"], default="full"),
+        tag_filters=dict(type="dict", required=False),
+        tag_filter_query=dict(type="str", required=False),
+    )
+
+    module = FederatedRealmInfoModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+        mutually_exclusive=[
+            ["id", "filters", "filter_query"],
+            ["id", "tag_filters", "tag_filter_query"],
+        ],
+    )
+    module.run_command()
+
+
+if __name__ == "__main__":
+    main()
