0a7e013e9b9801603bc2d217bd964395b0d71021
JchhatbarInfoblox <jchhatbar@infoblox.com>
2025-04-06 22:29:51 +0530

    added support for next availble subnet by tag filter

--- plugins/modules/ipam_next_available_subnet_info.py
+++ plugins/modules/ipam_next_available_subnet_info.py
@@ -20,7 +20,7 @@ options:
         description:
             - An application specific resource identity of a resource
         type: str
-        required: true
+        required: false
     cidr:
         description:
             - The cidr value of address blocks to be created.
@@ -30,6 +30,11 @@ options:
         description:
             - Number of address blocks to generate. Default 1 if not set.
         type: int
+        required: false
+    tag_filters:
+        description:
+            - Filter dict to filter address blocks by tags
+        type: dict
         required: false
 
 extends_documentation_fragment:
@@ -61,6 +66,29 @@ EXAMPLES = r"""
         cidr: 24
         count: 5
 
+    - name: "Create an Address Block with tags"
+      infoblox.universal_ddi.ipam_address_block:
+        address: "192.168.0.0/16"
+        space: "{{ ip_space.id }}"
+        tags:
+          environment: "production"
+          location: "data-center-1"
+        state: "present"
+
+    - name: "Get next available subnet by a single tag filter"
+      infoblox.universal_ddi.ipam_next_available_subnet_info:
+        tag_filters:
+          environment: "production"
+        cidr: 24
+        count: 1
+
+    - name: "Get next available subnet by multiple tag filters"
+      infoblox.universal_ddi.ipam_next_available_subnet_info:
+        tag_filters:
+          environment: "production"
+          location: "data-center-1"
+        cidr: 28
+        count: 5
 """
 
 RETURN = r"""
@@ -92,6 +120,43 @@ class NextAvailableSubnetInfoModule(UniversalDDIAnsibleModule):
         self._existing = None
         self._limit = 1000
 
+        # Validate count parameter if provided
+        if self.params.get("count") is not None and self.params.get("count") > 20:
+            self.fail_json(msg="Parameter 'count' cannot exceed 20")
+
+    def find_subnet(self, id=None, count=None):
+        try:
+            resp = AddressBlockApi(self.client).list_next_available_subnet(id=id, cidr=self.params["cidr"], count=count)
+            return resp.results
+        except ApiException:
+            return None
+
+    def find_address_blocks_by_tags(self):
+        tag_filter_str = None
+        if self.params["tag_filters"]:
+            tag_filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["tag_filters"].items()])
+
+        offset = 0
+        all_results = []  # Initialize a list to accumulate all results
+
+        while True:
+            try:
+                resp = AddressBlockApi(self.client).list(
+                    offset=offset, limit=self._limit, tfilter=tag_filter_str, inherit="full"
+                )
+
+                # Accumulate results from this page
+                all_results.extend(resp.results)
+
+                if len(resp.results) < self._limit:
+                    break
+                offset += self._limit
+
+            except ApiException as e:
+                self.fail_json(msg=f"Failed to execute command: {e.status} {e.reason} {e.body}")
+
+        return all_results
+
     def find(self):
         all_results = []
         offset = 0
@@ -118,12 +183,31 @@ class NextAvailableSubnetInfoModule(UniversalDDIAnsibleModule):
         if self.check_mode:
             self.exit_json(**result)
 
-        find_results = self.find()
+        if self.params.get("tag_filters"):
+            address_blocks = self.find_address_blocks_by_tags()
+            if not address_blocks:
+                self.fail_json(msg="No address block found with the given tags.")
+
+            find_results = []
+            for ab in address_blocks:
+                remaining_count = self.params.get("count", 1) - len(find_results)
+                while len(find_results) < self.params.get("count", 1):
+                    find_result = self.find_subnet(id=ab.id, count=remaining_count)
+                    if find_result:
+                        find_results.extend(find_result)
+                        break
+                    else:
+                        remaining_count -= 1
+                        if not remaining_count:
+                            break
+
+            if len(find_results) < self.params.get("count", 1):
+                self.fail_json(msg="Not enough subnets found with the given tags.")
+        else:
+            find_results = self.find()
 
         all_results = []
         for r in find_results:
-            # The expected output is a list of addresses as strings.
-            # Therefore, we extract only the 'address' field from each object in the results.
             all_results.append(r.address)
 
         result["objects"] = all_results
@@ -133,14 +217,17 @@ class NextAvailableSubnetInfoModule(UniversalDDIAnsibleModule):
 def main():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        id=dict(type="str", required=True),
+        id=dict(type="str", required=False),
         cidr=dict(type="int", required=True),
         count=dict(type="int", required=False),
+        tag_filters=dict(type="dict", required=False),
     )
 
     module = NextAvailableSubnetInfoModule(
         argument_spec=module_args,
         supports_check_mode=True,
+        required_one_of=[["id", "tag_filters"]],
+        mutually_exclusive=[["id", "tag_filters"]],
     )
     module.run_command()
 
