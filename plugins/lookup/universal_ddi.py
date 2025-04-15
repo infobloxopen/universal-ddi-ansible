12071668f934ca8673807b477f23528b26e72aab
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-04-15 13:10:44 +0530

    Modified Lookup Plugin for UDDI

--- plugins/lookup/universal_ddi.py
+++ plugins/lookup/universal_ddi.py
@@ -9,13 +9,11 @@ __metaclass__ = type
 DOCUMENTATION = """
 ---
 name: universal_ddi
-author:
-  - "Vedant Sethia (@vedantsethia)"
-  - "Chris Marrison (@ccmarris)"
+author: Infoblox Inc. (@infobloxopen)
 short_description: Query Infoblox BloxOne DDI objects
 version_added: "1.1.0"
 description:
-  - Uses the BloxOne DDI REST API to fetch BloxOne specified objects.  This lookup
+  - Uses the Universal DDI APIs to fetch specific objects.  This lookup
     supports adding additional keywords to filter the return data and specify
     the desired set of returned fields.
 requirements:
@@ -23,7 +21,7 @@ requirements:
 
 options:
     _terms:
-      description: The name of the object to return from BloxOne
+      description: The name of the object to returned from Universal DDI API
       required: True
     fields:
       description: The list of field names to return for the specified object.
@@ -32,13 +30,13 @@ options:
     tfilters:
       description: a dict object that is used to filter the return objects based on tags
     provider:
-      description: a dict object containing BloxOne host name and API key
+      description: a dict object containing Universal DDI Portal URL and key
 """
 
 EXAMPLES = """
 - name: fetch all IP Space objects
   ansible.builtin.set_fact:
-    ip_space: "{{ lookup('universal_ddi', '/ipam/ipspace' , filters={'name': 'vsethia-ip-space'}, tfilters={'Tagname': '<value>'}, fields=['id', 'name', 'comment'] , provider={'host': '{{host}}', 'api_key': '{{api_key}}'}) }}"
+    ip_space: "{{ lookup('infoblox.universal_ddi.universal_ddi','ipam/ipspace', fields=['id', 'name', 'comment'] , provider={'portal_url': 'https://stage.csp.infoblox.com', 'portal_key': 'c96c7ad83735f70c0ad95e1a68684cc07b5566b06e6227f481267dc1593f4d48'}) }}"
 
 """  # noqa: E501
 
@@ -59,11 +57,27 @@ else:
     REQUESTS_LIB_IMP_ERR = None
 
 
+def return_base_url(obj_type):
+    """Returns the base URL for the object type"""
+    base_obj_type = obj_type.split("/")[0]
+    if base_obj_type == "anycast":
+        return "/api/anycast/v1"
+    elif base_obj_type == "cloud_discovery":
+        return "/api/cloud_discovery/v2"
+    elif base_obj_type == "dns_config" or base_obj_type == "dns_data" or base_obj_type == "ipam" or base_obj_type == "dhcp" or base_obj_type == "ipam_federation" or base_obj_type == "keys":
+        return "/api/ddi/v1"
+    elif base_obj_type == "infra_mgmt":
+        return "/api/infra/v1"
+    elif base_obj_type == "infra_provision":
+        return "/api/host-activation/v1"
+    else:
+        raise AnsibleError(f"Invalid object type: {obj_type}")
+
 def get_object(obj_type, provider, filters, tfilters, fields):
     """Creating the GET API request for lookup"""
     try:
-        host = provider["host"]
-        key = provider["api_key"]
+        portal_url = provider["portal_url"]
+        portal_key = provider["portal_key"]
     except KeyError:
         return (
             True,
@@ -74,7 +88,7 @@ def get_object(obj_type, provider, filters, tfilters, fields):
                 "provider": provider,
             },
         )
-    endpoint = f"/api/ddi/v1/{obj_type}"
+    endpoint = f"{return_base_url(obj_type)}/{obj_type}"
     flag = 0
     if fields is not None and isinstance(fields, list):
         temp_fields = ",".join(fields)
@@ -93,6 +107,7 @@ def get_object(obj_type, provider, filters, tfilters, fields):
             endpoint = endpoint + "&_filter=" + res
         else:
             endpoint = endpoint + "?_filter=" + res
+            flag = 1
 
     if tfilters != {} and isinstance(tfilters, dict):
         temp_tfilters = []
@@ -109,19 +124,19 @@ def get_object(obj_type, provider, filters, tfilters, fields):
 
     # reproduced module_utils. Replace once published
     try:
-        headers = {"Authorization": f"Token {key}"}
-        url = f"{host}{endpoint}"
+        headers = {"Authorization": f"Token {portal_key}"}
+        url = f"{portal_url}{endpoint}"
         result = requests.get(url, headers=headers)
     except Exception:
         raise Exception("API request failed")
 
     if result.status_code in [200, 201, 204]:
-        return (False, False, result.json())
+        return False, False, result.json()
     elif result.status_code == 401:
-        return (True, False, result.content)
+        return True, False, result.content
     else:
         meta = {"status": result.status_code, "response": result.json()}
-        return (True, False, meta)
+        return True, False, meta
 
 
 class LookupModule(LookupBase):
@@ -129,7 +144,7 @@ class LookupModule(LookupBase):
         if not HAS_REQUESTS_LIB:
             raise AnsibleError(
                 "The 'universal_ddi' lookup cannot be run without the 'requests' library installed."
-            ) from REQUESTS_LIB_IMP_ERR
+            )(REQUESTS_LIB_IMP_ERR)
         try:
             obj_type = terms[0]
         except IndexError:
