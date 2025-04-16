eeaccff418f8d195610cb7dccd426ee7d771028c
JchhatbarInfoblox <jchhatbar@infoblox.com>
2025-04-16 20:53:10 +0530

    addressed review comments

--- plugins/modules/ipam_next_available_ip_info.py
+++ plugins/modules/ipam_next_available_ip_info.py
@@ -43,7 +43,6 @@ options:
     resource_type:
         description:
             - "Type of resource to filter when using tag_filters. Required when tag_filters is provided."
-            - "Options are 'address_block', 'subnet', or 'range'."
         type: str
         required: false
         choices:
@@ -65,6 +64,13 @@ EXAMPLES = r"""
       infoblox.universal_ddi.ipam_next_available_ip_info:
         id: "{{ _address_block.id }}"
 
+    - name: Get Information about Next Available IP in Address Blocks filtered by tags
+      infoblox.universal_ddi.ipam_next_available_ip_info:
+        tag_filters:
+          environment: "production"
+        resource_type: "address_block"
+        count: 5
+
     - name: Get Information about Next Available IP in Subnet
       infoblox.universal_ddi.ipam_next_available_ip_info:
         id: "{{ _subnet.id }}"
@@ -73,6 +79,13 @@ EXAMPLES = r"""
     - name: Get Information about Next Available IP in Subnet Default Count
       infoblox.universal_ddi.ipam_next_available_ip_info:
         id: "{{ _subnet.id }}"
+
+    - name: Get Information about Next Available IP in Subnets filtered by tags
+      infoblox.universal_ddi.ipam_next_available_ip_info:
+        tag_filters:
+          environment: "production"
+        resource_type: "subnet"
+        count: 5
         
     - name: Get Information about Next Available IP in Range
       infoblox.universal_ddi.ipam_next_available_ip_info:
@@ -82,19 +95,12 @@ EXAMPLES = r"""
     - name: Get Information about Next Available IP in Range Default Count
       infoblox.universal_ddi.ipam_next_available_ip_info:
         id: "{{ _range.id }}"
-   
-    - name: Get Information about Next Available IP in Address Blocks filtered by tags
+
+    - name: Get Information about Next Available IP in Range filtered by tags
       infoblox.universal_ddi.ipam_next_available_ip_info:
         tag_filters:
           environment: "production"
-        resource_type: "address_block"
-        count: 5
-        
-    - name: Get Information about Next Available IP in Subnets filtered by tags
-      infoblox.universal_ddi.ipam_next_available_ip_info:
-        tag_filters:
-          environment: "production"
-        resource_type: "subnet"
+        resource_type: "range"
         count: 5
 """  # noqa: E501
 
@@ -127,6 +133,35 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
         self._existing = None
         self._limit = 1000
 
+    def _get_api_for_resource(self, resource_id=None, resource_type=None):
+        """
+        Helper function to get the appropriate API based on resource type or ID.
+
+        Args:
+            resource_id (str, optional): The resource ID to determine type from
+            resource_type (str, optional): Explicit resource type (address_block, subnet, or range)
+
+        Returns:
+            API class: The appropriate API class for the resource type
+        """
+        if resource_id and "/" in resource_id:
+            address_str = resource_id.rsplit("/", 1)[0]
+            if address_str == "ipam/address_block" or resource_type == "address_block":
+                return AddressBlockApi(self.client)
+            elif address_str == "ipam/subnet" or resource_type == "subnet":
+                return SubnetApi(self.client)
+            elif address_str == "ipam/range" or resource_type == "range":
+                return RangeApi(self.client)
+        elif resource_type:
+            if resource_type == "address_block":
+                return AddressBlockApi(self.client)
+            elif resource_type == "subnet":
+                return SubnetApi(self.client)
+            elif resource_type == "range":
+                return RangeApi(self.client)
+
+        raise ValueError("Unable to determine resource type")
+
     def find(self):
         all_results = []
         offset = 0
@@ -134,21 +169,12 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
         while True:
             try:
                 id = self.params["id"]
-                address_str = id.rsplit("/", 1)[0]
-                if address_str == "ipam/address_block":
-                    resp = AddressBlockApi(self.client).list_next_available_ip(
-                        id=id, contiguous=self.params["contiguous"], count=self.params["count"]
-                    )
-                elif address_str == "ipam/subnet":
-                    resp = SubnetApi(self.client).list_next_available_ip(
-                        id=id, contiguous=self.params["contiguous"], count=self.params["count"]
-                    )
-                elif address_str == "ipam/range":
-                    resp = RangeApi(self.client).list_next_available_ip(
-                        id=id, contiguous=self.params["contiguous"], count=self.params["count"]
-                    )
-
-                all_results.extend(resp.results)
+                client = self._get_api_for_resource(resource_id=id)
+                resp = client.list_next_available_ip(
+                    id=id, contiguous=self.params["contiguous"], count=self.params["count"]
+                )
+                if resp.results:
+                    all_results.extend(resp.results)
 
                 if len(resp.results) < self._limit:
                     break
@@ -161,46 +187,26 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
 
     def find_next_available_ip(self, id=None, resource_type=None, count=None):
         try:
-            if resource_type == "address_block" or ("ipam/address_block" in id):
-                resp = AddressBlockApi(self.client).list_next_available_ip(
-                    id=id, contiguous=self.params["contiguous"], count=count
-                )
-            elif resource_type == "subnet" or ("ipam/subnet" in id):
-                resp = SubnetApi(self.client).list_next_available_ip(
-                    id=id, contiguous=self.params["contiguous"], count=count
-                )
-            elif resource_type == "range" or ("ipam/range" in id):
-                resp = RangeApi(self.client).list_next_available_ip(
-                    id=id, contiguous=self.params["contiguous"], count=count
-                )
+            client = self._get_api_for_resource(resource_id=id, resource_type=resource_type)
+            resp = client.list_next_available_ip(id=id, contiguous=self.params["contiguous"], count=count)
             return resp.results
-        except ApiException:
+
+        except (ApiException, ValueError):
             return None
 
     def find_resources_by_tags(self, resource_type):
-        tag_filter_str = None
-        if self.params["tag_filters"]:
-            tag_filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["tag_filters"].items()])
+        tag_filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["tag_filters"].items()])
 
         offset = 0
         all_resources = []
 
         while True:
             try:
-                if resource_type == "address_block":
-                    resp = AddressBlockApi(self.client).list(
-                        offset=offset, limit=self._limit, tfilter=tag_filter_str, inherit="full"
-                    )
-                elif resource_type == "subnet":
-                    resp = SubnetApi(self.client).list(
-                        offset=offset, limit=self._limit, tfilter=tag_filter_str, inherit="full"
-                    )
-                elif resource_type == "range":
-                    resp = RangeApi(self.client).list(
-                        offset=offset, limit=self._limit, tfilter=tag_filter_str, inherit="full"
-                    )
+                api = self._get_api_for_resource(resource_type=resource_type)
+                resp = api.list(offset=offset, limit=self._limit, tfilter=tag_filter_str, inherit="full")
 
-                all_resources.extend(resp.results)
+                if resp.results:
+                    all_resources.extend(resp.results)
 
                 if len(resp.results) < self._limit:
                     break
@@ -217,9 +223,11 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
         if self.check_mode:
             self.exit_json(**result)
 
+        count = self.params["count"]
+
         # Validate count is within allowed range
-        if not 1 <= self.params["count"] <= 20:
-            self.fail_json(msg="count must be between 1 and 20")
+        if not 1 <= count <= 20:
+            self.fail_json(msg="count must be between 1 and 20.")
 
         if self.params["tag_filters"]:
             if not self.params["resource_type"]:
@@ -232,12 +240,21 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
                 self.fail_json(msg=f"No {resource_type}s found with the given tags.")
 
             find_results = []
-            for ab in resources:
-                remaining_count = self.params["count"] - len(find_results)
+            for resource in resources:
 
-                while len(find_results) < self.params["count"]:
+                # Check if the resource has next available ip
+                if count > 1:
+                    check_result = self.find_next_available_ip(
+                        id=resource.id, resource_type=self.params["resource_type"], count=1
+                    )
+                    if not check_result:
+                        continue
+
+                remaining_count = count - len(find_results)
+
+                while len(find_results) < count:
                     find_result = self.find_next_available_ip(
-                        id=ab.id, resource_type=self.params["resource_type"], count=remaining_count
+                        id=resource.id, resource_type=self.params["resource_type"], count=remaining_count
                     )
 
                     if find_result:
@@ -248,7 +265,7 @@ class NextAvailableIPInfoModule(UniversalDDIAnsibleModule):
                         if not remaining_count:
                             break
 
-            if len(find_results) < self.params["count"]:
+            if len(find_results) < count:
                 self.fail_json(msg=f"Not enough available IPs found in {resource_type}s with the given tags.")
         else:
             find_results = self.find()
