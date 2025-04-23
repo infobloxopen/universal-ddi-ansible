d5dce6e682234425f1b4b34d931b628b2a3495c7
JchhatbarInfoblox <jchhatbar@infoblox.com>
2025-04-23 22:06:58 +0530

    Handled error msg and changed logic to obtain next available ab

--- plugins/modules/ipam_next_available_address_block_info.py
+++ plugins/modules/ipam_next_available_address_block_info.py
@@ -5,6 +5,9 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 
+import json
+import re
+
 __metaclass__ = type
 
 DOCUMENTATION = r"""
@@ -117,6 +120,36 @@ class NextAvailableAddressBlockInfoModule(UniversalDDIAnsibleModule):
         if (self.params["count"] is not None) and (self.params["count"] <= 0 or self.params["count"] > 20):
             self.fail_json(msg="Parameter 'count' must be between 1 and 20")
 
+    def extract_available_count_from_error(self, error_body):
+        """
+        Extract the available count from an API error message.
+
+        This function parses the error response when the API returns an error about
+        having fewer available networks than requested.
+
+        :param error_body: The error response body from the API
+        :return: The number of available networks (int), or 0 if the count couldn't be extracted
+        """
+        available_count = 0
+        try:
+            # Parse the JSON error body
+            error_json = json.loads(error_body)
+            if "error" in error_json and len(error_json["error"]) > 0:
+                error_message = error_json["error"][0]["message"]
+
+                # Use regex to extract the number after "The available networks are: "
+                match = re.search(r"The available networks are: (\d+)", error_message)
+                if match:
+                    available_count = int(match.group(1))
+                else:
+                    # If regex fails send fail message
+                    self.fail_json(msg=f"{error_message}")
+
+        except (json.JSONDecodeError, KeyError, IndexError, ValueError) as parse_error:
+            self.fail_json(msg=f"Failed to parse error body: {parse_error}")
+
+        return available_count
+
     def find(self):
         all_results = []
         offset = 0
@@ -139,9 +172,12 @@ class NextAvailableAddressBlockInfoModule(UniversalDDIAnsibleModule):
     def find_address_block(self, id=None, count=None):
         try:
             resp = AddressBlockApi(self.client).list_next_available_ab(id=id, cidr=self.params["cidr"], count=count)
-
             return resp.results
-        except ApiException:
+        except ApiException as e:
+            # If it's a "fewer than requested" error, extract available count
+            if e.status == 400:
+                available_count = self.extract_available_count_from_error(e.body)
+                return available_count
             return None
 
     def run_command(self):
@@ -159,29 +195,34 @@ class NextAvailableAddressBlockInfoModule(UniversalDDIAnsibleModule):
 
             find_results = []
             for ab in address_blocks:
-
-                # Check if the address block have the free address block
-                if count > 1:
-                    check_result = self.find_address_block(id=ab.id, count=1)
-                    if not check_result:
-                        continue
+                if len(find_results) >= count:
+                    break
 
                 remaining_count = count - len(find_results)
-                while len(find_results) < count:
-                    find_result = self.find_address_block(id=ab.id, count=remaining_count)
-                    if find_result:
-                        find_results.extend(find_result)
-                        break
-                    else:
-                        remaining_count -= 1
-                        if not remaining_count:
-                            break
+                find_result = self.find_address_block(id=ab.id, count=remaining_count)
 
-            # Move the check for insufficient addresses outside of the loop
+                if isinstance(find_result, int):
+                    # We got back an available count from error parsing
+                    if find_result == 0:
+                        # No addresses available in this block
+                        continue
+                    elif find_result < remaining_count:
+                        # We got fewer than requested, so we need to adjust the count
+                        # and try to find more addresses
+                        find_results.extend(self.find_address_block(id=ab.id, count=find_result))
+
+                elif find_result:
+                    # We got the requested results
+                    find_results.extend(find_result)
+
+            if len(find_results) == 0:
+                self.fail_json(msg="No address blocks available with the given tags and CIDR.")
+            # we couldn't find all requested address blocks
             if len(find_results) < count:
-                self.fail_json(msg="Not enough address blocks found with the given tags.")
+                self.fail_json(msg=f"Requested {count} address blocks but only {len(find_results)} were available")
 
         else:
+            # Direct ID lookup
             find_results = self.find()
 
         result["objects"] = [r.address for r in find_results]
