4a3c498dad60d53937ce2a253db2272684b6d752
Rishabh Singh <rsingh4@infoblox.com>
2026-06-02 22:17:33 +0530

    updated user security model

---
+++ plugins/modules/dtc_snmp_user_security_model_info.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: Infoblox Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: dtc_snmp_user_security_model_info
+short_description: Retrieve SnmpUserSecurity information
+description:
+    - Retrieve information about SnmpUserSecurity
+version_added: 1.0.0
+author: Infoblox Inc. (@infobloxopen)
+options:
+    id:
+        description:
+            - ID of the object
+        type: str
+        required: false
+    filters:
+        description:
+            - Filter dict to filter objects
+        type: dict
+        required: false
+    filter_query:
+        description:
+            - Filter query to filter objects
+        type: str
+        required: false
+    inherit:
+        description:
+            - Return inheritance information
+        type: str
+        required: false
+        choices:
+            - full
+            - partial
+            - none
+        default: full
+    tag_filters:
+        description:
+            - Filter dict to filter objects by tags
+        type: dict
+        required: false
+    tag_filter_query:
+        description:
+            - Filter query to filter objects by tags
+        type: str
+        required: false
+
+extends_documentation_fragment:
+    - infoblox.universal_ddi.common
+"""  # noqa: E501
+
+EXAMPLES = r"""
+- name: Get all SNMP User Security Model objects
+  infoblox.universal_ddi.dtc_snmp_user_security_model_info:
+
+- name: Get SNMP User Security Model by ID
+  infoblox.universal_ddi.dtc_snmp_user_security_model_info:
+    id: "{{ usm_id }}"
+
+- name: Get SNMP User Security Model by username filter
+  infoblox.universal_ddi.dtc_snmp_user_security_model_info:
+    filters:
+      username: "snmpuser"
+
+- name: Get SNMP User Security Model by filter query
+  infoblox.universal_ddi.dtc_snmp_user_security_model_info:
+    filter_query: "username=='snmpuser'"
+"""
+
+RETURN = r"""
+id:
+    description:
+        - ID of the SnmpUserSecurity object
+    type: str
+    returned: Always
+objects:
+    description:
+        - SnmpUserSecurity object
+    type: list
+    elements: dict
+    returned: Always
+    contains:
+        auth_passphrase:
+            description:
+                - "User passphrase for authentication. Ignored for B(NoAuth), otherwise mandatory."
+            type: str
+            returned: Always
+        auth_protocol:
+            description:
+                - "Authentication protocol."
+                - "Allowed values:"
+                - "* NoAuth"
+                - "* MD5"
+                - "* SHA"
+                - "Defaults to B(NoAuth)."
+            type: str
+            returned: Always
+        id:
+            description:
+                - "The resource identifier."
+            type: str
+            returned: Always
+        privacy_passphrase:
+            description:
+                - "User passphrase for privacy. Ignored for B(NoPrivacy), otherwise mandatory."
+            type: str
+            returned: Always
+        privacy_protocol:
+            description:
+                - "Privacy protocol. Must be B(NoPrivacy) if auth_protocol set to B(NoAuth)."
+                - "Allowed values:"
+                - "* NoPrivacy"
+                - "* DES"
+                - "* AES"
+                - "Defaults to B(NoPrivacy)."
+            type: str
+            returned: Always
+        username:
+            description:
+                - "User name with which to associate security information."
+            type: str
+            returned: Always
+"""  # noqa: E501
+
+from ansible_collections.infoblox.universal_ddi.plugins.module_utils.modules import UniversalDDIAnsibleModule
+
+try:
+    from dtc import SnmpUserSecurityApi
+    from universal_ddi_client import ApiException, NotFoundException
+except ImportError:
+    pass  # Handled by UniversalDDIAnsibleModule
+
+
+class SnmpUserSecurityInfoModule(UniversalDDIAnsibleModule):
+    def __init__(self, *args, **kwargs):
+        super(SnmpUserSecurityInfoModule, self).__init__(*args, **kwargs)
+        self._existing = None
+        self._limit = 1000
+
+    def find_by_id(self):
+        try:
+            resp = SnmpUserSecurityApi(self.client).read(self.params["id"], inherit="full")
+            return [resp.result]
+        except NotFoundException as e:
+            return None
+
+    def find(self):
+        if self.params["id"] is not None:
+            return self.find_by_id()
+
+        filter_str = None
+        if self.params["filters"] is not None:
+            filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["filters"].items()])
+        elif self.params["filter_query"] is not None:
+            filter_str = self.params["filter_query"]
+
+        tag_filter_str = None
+        if self.params["tag_filters"] is not None:
+            tag_filter_str = " and ".join([f"{k}=='{v}'" for k, v in self.params["tag_filters"].items()])
+        elif self.params["tag_filter_query"] is not None:
+            tag_filter_str = self.params["tag_filter_query"]
+
+        all_results = []
+        offset = 0
+
+        while True:
+            try:
+                resp = SnmpUserSecurityApi(self.client).list(
+                    offset=offset, limit=self._limit, filter=filter_str, tfilter=tag_filter_str
+                )
+
+                # If no results, set results to empty list
+                if not resp.results:
+                    resp.results = []
+
+                all_results.extend(resp.results)
+
+                if len(resp.results) < self._limit:
+                    break
+                offset += self._limit
+
+            except ApiException as e:
+                self.fail_json(msg=f"Failed to execute command: {e.status} {e.reason} {e.body}")
+
+        return all_results
+
+    def run_command(self):
+        result = dict(objects=[])
+
+        if self.check_mode:
+            self.exit_json(**result)
+
+        find_results = self.find()
+
+        all_results = []
+        for r in find_results:
+            all_results.append(r.model_dump(by_alias=True, exclude_none=True))
+
+        result["objects"] = all_results
+        self.exit_json(**result)
+
+
+def main():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        id=dict(type="str", required=False),
+        filters=dict(type="dict", required=False),
+        filter_query=dict(type="str", required=False),
+        inherit=dict(type="str", required=False, choices=["full", "partial", "none"], default="full"),
+        tag_filters=dict(type="dict", required=False),
+        tag_filter_query=dict(type="str", required=False),
+    )
+
+    module = SnmpUserSecurityInfoModule(
+        argument_spec=module_args,
+        supports_check_mode=True,
+        mutually_exclusive=[
+            ["id", "filters", "filter_query"],
+            ["id", "tag_filters", "tag_filter_query"],
+        ],
+    )
+    module.run_command()
+
+
+if __name__ == "__main__":
+    main()
